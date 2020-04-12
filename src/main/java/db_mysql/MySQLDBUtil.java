159057761e483d439c78cd56b473fb3140f2aa70
Zifan Zhang <zyku.779@gmail.com>
2020-04-11 20:11:18 -0400

    Update MySQLDBUtil.java

--- src/main/java/db_mysql/MySQLDBUtil.java
+++ src/main/java/db_mysql/MySQLDBUtil.java
@@ -2,11 +2,11 @@ package db_mysql;
 
 public class MySQLDBUtil {
 	
-	private static final String INSTANCE = "zifan-instance.cuqs3l5shg0m.us-east-2.rds.amazonaws.com";
+	private static final String INSTANCE = "";
 	private static final String PORT_NUM = "3306";
-	public static final String DB_NAME = "Zifan_database";
+	public static final String DB_NAME = "database";
 	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "Value!779";
+	private static final String PASSWORD = "";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
